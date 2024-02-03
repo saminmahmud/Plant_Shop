400ce72813f4555198cd02b47f49fc276b6c16c0
saminmahmud <samin.mahmud18@gmail.com>
2024-02-03 19:34:35 +0600

    settings.py rest_framework added

--- plant_shop/settings.py
+++ plant_shop/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
